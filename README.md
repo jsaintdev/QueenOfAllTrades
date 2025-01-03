7b1b0509041bf92fd4b33f38627147d15d363499
Justin Saint <justinsaintdev@gmail.com>
2025-01-03 13:18:51 -0800

    Defined test routes and testing images

--- README.md
+++ README.md
@@ -1,10 +1,28 @@
 # QueenOfAllTrades
 Interactive game inspired by the Drag Race franchise
 
-v1.0 - Setting up the Log-in Screen
-===================================
+v1.0 - Setting up test routes and images
+========================================
+- Set up an "about" page that should display my name
+- To test:
+open browser at: https://localhost:3000/about
+
+- Set up test images using DR Queen profiles:
+- To Test:
+open browser at: https://localhost:3000/queen/1
+/1 to /4 should display Queens
+/5 or more should display an error message 
+
+v1.0 - Setting up the Server
+=============================
 - Set up a simple server using Express.js
 - Set up Node.js project configuration
 
-- To test:
-open browser at: https://localhost:3000
+- To install:
+1. npm init
+2. npm install
+3. npm install express
+
+- To run:
+1. nodemon index.js
+2. open browser at: https://localhost:3000
--- index.js
+++ index.js
@@ -11,12 +11,46 @@ const port = process.env.PORT || 3000;
 // Initialize instace of Express app
 const app = express();
 
-// Defines route for HTTP GET Requests
+/* 
+    Defines routes for HTTP GET Requests
+*/
 app.get('/', (req,res) => {
     res.send("<h1>Hello World!</h1>");
 });
 
-// Starts the server and listens for incoming requests on the specified port
+app.get('/about', (req,res) => {
+    var color = req.query.color;
+
+    res.send("<h1 style='color:"+color+";'>Justin</h1>");
+});
+
+app.get('/queen/:id', (req,res) => {
+
+    var queen = req.params.id;
+
+    if (queen == 1) {
+        res.send("Jinkx: <img src='/Jinkx.webp' style='width:250px;'>");
+    }
+    else if (queen == 2) {
+        res.send("Marina: <img src='/Marina.webp' style='width:250px;'>");
+    }
+    else if (queen == 3) {
+        res.send("Nymphia: <img src='/Nymphia.webp' style='width:250px;'>");
+    }
+    else if (queen == 4) {
+        res.send("Sasha: <img src='/Sasha.webp' style='width:250px;'>");
+    }
+    else {
+        res.send("Invalid Queen id: "+queen);
+    }
+});
+
+
+app.use(express.static(__dirname + "/public"));
+
+/*
+    Starts the server and listens for requests on the port
+*/
 app.listen(port, () => {
     console.log("Node application listening on port "+port);
 }); 
